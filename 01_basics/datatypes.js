18a74269a59d9d67a2f28828645a9371c48959c2
ameesha25 <ameeshabaghel2509@gmail.com>
2024-09-09 06:58:54 +0000

    datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,3 @@
+"use strict"; //treat JS as a newer version
+// alert(3+3) we are using Node js not browser 
+
--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,13 @@
-console.log("Ameesha")
+const accountId=1445678;
+let accountEmail="amishabaghek276@gmail.com";
+var accountPassword="128765"
+accountCity="jaipur"
+
+// console.log(accountId)
+
+accountEmail="hc@hcgmail.com"
+accountPassword="25143257"
+accountCity="bengluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
