5c04884e44a5f4275617c1ff7d3d6b623c8d0bab
ameesha25 <ameeshabaghel2509@gmail.com>
2024-09-09 18:15:04 +0000

    datatype summary

---
+++ 01_basics/datatypesummaryjs.js
@@ -0,0 +1,35 @@
+//primitive and non primitive 
+
+//primitive 
+//7 types of datatype
+// string, boolean, number,null,undefined,symbol,bigInt 
+
+
+
+// const score=100
+// const scoreValue=100.3
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail;
+
+const id= Symbol("123")
+const anotherId=Symbol("123")
+// console.log(id=== anotherId); //false
+
+// const bigNumber=763875874864n
+
+//reference non primitive 
+// Array,objects,functions
+
+const heros=["Shaktiman","nagraj","doga"];
+
+let myObj={
+    name:"Ameesha",
+    age:"21"
+}
+
+const myFumction=function(){
+    console.log("Hello world ");
+}
+
+console.log(typeof bigNumber) //undefined
