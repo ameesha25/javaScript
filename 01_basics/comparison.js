54bd80e629ccf9f94d369f24e0c38ec79560c60a
ameesha25 <ameeshabaghel2509@gmail.com>
2024-09-09 17:54:06 +0000

    comparison of datatype

---
+++ 01_basics/comparison.js
@@ -0,0 +1,12 @@
+console.log("02">1)///true
+console.log("2">1)//true
+console.log(null>"0") //false
+
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+// ===(this strictly check the datatypes)
