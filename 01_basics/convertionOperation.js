7c2759f54380c80d83cef2c255e4f0a8c544aae2
ameesha25 <ameeshabaghel2509@gmail.com>
2024-09-09 10:02:44 +0000

    convertion operation

---
+++ 01_basics/convertionOperation.js
@@ -0,0 +1,23 @@
+let score="Ameesha"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc"=>NaN not a number
+
+
+let isLoggedIn="";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// true=> 1;false=>0
+// ""=>false
+// "hitesh"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,3 +1,14 @@
 "use strict"; //treat JS as a newer version
 // alert(3+3) we are using Node js not browser 
 
+//number 
+//bigInt
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+//object 
+
+// typeof null is object ... language error 
