97d5aa492e4f96f70f7a655d6041459dc16d3d15
ameesha25 <ameeshabaghel2509@gmail.com>
2024-09-09 10:35:06 +0000

    string to number convertion

--- 01_basics/convertionOperation.js
+++ 01_basics/convertionOperation.js
@@ -20,4 +20,29 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+// console.log(3+3);
+// console.log(2-2);
+// console.log(2*2);
+
+let str1="Ameesha "
+let str2="Baghel"
+
+let str3=str1+str2;
+// console.log(str3)
+// console.log((2+3)*5%7);
+
+num1=num2=num3=2+2
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
