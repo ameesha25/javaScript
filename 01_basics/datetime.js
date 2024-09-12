dae3ed66d5dde2aebcdd6cfc903932d69099f7f9
ameesha25 <ameeshabaghel2509@gmail.com>
2024-09-12 11:04:56 +0000

    Date and time in js

---
+++ 01_basics/datetime.js
@@ -0,0 +1,43 @@
+// DATES 
+let myDate= new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate=new Date(2023,0,23);
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate=new Date("01-14-2023");
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate= new Date(2023,0,23,5,3);
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate=new Date("2023-01-14");
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate=new Date("01-14-2023")
+// let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+// let newDate= new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekend:"long",
+
+
+})
+
