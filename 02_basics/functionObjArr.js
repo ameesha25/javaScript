8a8dc61d736d4d5aef4d95def8d57abbb310c2b2
ameesha25 <ameeshabaghel2509@gmail.com>
2024-09-17 08:01:17 +0000

    functions with object and array

---
+++ 02_basics/functionObjArr.js
@@ -0,0 +1,29 @@
+// function calculateCartPrice(val1,val2,...num1){ 
+//     //val1 ,val2 se starting ki values usme chlejati hai 
+//     return num1
+// }
+
+// console.log(calculateCartPrice(300,400,500,600,700)) //it will converted into array 
+
+// const user ={
+//     username:"Ameesha ",
+//     price:500
+
+// }
+
+// function handleObject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+
+// handleObject({
+//     username:"sam",
+//     price:499
+// })
+
+const myNewArray=[200,300,400,500];
+
+function returnSecondValue(getArray){
+    return  getArray[1]
+
+}
+console.log(returnSecondValue([200,300,400,500]));
