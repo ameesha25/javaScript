708c2c5bba344c9062fb020d24b303be85ccb3a2
ameesha25 <ameeshabaghel2509@gmail.com>
2024-09-18 12:54:10 +0000

    control flow

---
+++ control_flow/one.js
@@ -0,0 +1,29 @@
+// if(2=="2"){
+//     console.log("executed");
+// }
+
+// const score=200
+
+// if(score>100){
+//     const power= "fly "
+//     console.log(`user power: ${power}`);
+// }
+
+const balance=1000;
+// if(balance>500) console.log("test"); //it is implicit scope and execcutes on a single line
+
+////falsy value 
+//  false,0 , BigInt, -0, 0n, "",null, undefined,NaN
+
+///truthy value 
+// "0"," ",'false', function(){},{},[]
+
+// nullish coalescing operator (??): null undefined
+let val1;
+val1= 5??10
+val2=null??54
+val3=undefined??87
+
+console.log(val1);
+console.log(val2);
+console.log(val3);
